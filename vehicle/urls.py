a2c682364c02a9561cfd07a44f5a25d1f28f143e
IronMan2028 <ironman2028jf@gmail.com>
2024-06-12 10:31:56 +0530

    Delete vehicle directory

--- vehicle/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import VehicleListCreate
-
-urlpatterns = [
-    path('vehicles/', VehicleListCreate.as_view(), name='vehicle-list-create'),
-]
--- vehicle/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Vehicle
-from .serializer import VehicleSerializer
-
-class VehicleListCreate(generics.ListCreateAPIView):
-    queryset = Vehicle.objects.all()
-    serializer_class = VehicleSerializer
